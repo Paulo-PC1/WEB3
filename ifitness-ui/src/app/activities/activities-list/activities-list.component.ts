6fc52c73f95dbf4a3de11434b4ec0c2ce589697a
Paulo-PC1 <paulocezareite91@gmail.com>
2026-08-06 20:41:47 -0300

    Aula dia 06/08/2026

---
+++ ifitness-ui/src/app/activities/activities-list/activities-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activities-list',
+  imports: [],
+  templateUrl: './activities-list.component.html',
+  styleUrl: './activities-list.component.css'
+})
+export class ActivitiesListComponent {
+
+}
